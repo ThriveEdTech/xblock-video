a14e2eb69beccc3b2022994b658d7fa8bea2549d
M. Hasan <kazilotus@users.noreply.github.com>
2020-07-25 16:30:20 +0600

    Update mixins.py

--- video_xblock/mixins.py
+++ video_xblock/mixins.py
@@ -624,5 +624,5 @@ class LocationMixin(XBlock):
         Returns stub value if `location` property is unavailabe. E.g. in workbench runtime.
         """
         if hasattr(self, 'location'):
-            return self.location.to_string()
+            return unicode(self.location)
         return 'usage_id'
